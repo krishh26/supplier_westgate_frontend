47b60f475dc8ec577e4301fbc1d233bc29cc7ff9
Kishansinh Parmar <kishansinhp26@gmail.com>
2024-10-27 14:06:16 +0530

    changes done

--- src/app/modules/supplier-admin/project-list-dashboard/project-list-dashboard.component.ts
+++ src/app/modules/supplier-admin/project-list-dashboard/project-list-dashboard.component.ts
@@ -204,7 +204,7 @@ export class ProjectListDashboardComponent implements OnInit {
     Payload.projectList.keyword = this.searchText;
     Payload.projectList.page = String(this.page);
     Payload.projectList.limit = String(this.pagesize);
-    Payload.projectList.status = this.status || ''
+    Payload.projectList.status = this.status || '';
     this.projectService.getProjectList(Payload.projectList).subscribe((response) => {
       this.projectList = [];
       this.totalRecords = response?.data?.meta_data?.items;
@@ -243,7 +243,7 @@ export class ProjectListDashboardComponent implements OnInit {
     Payload.projectList.industry = this.selectedIndustries.join(',');
     Payload.projectList.projectType = this.selectedProjectTypes.join(',');
     Payload.projectList.clientType = this.selectedClientTypes.join(',');
-    Payload.projectList.status = this.selectedStatuses.join(',')
+    Payload.projectList.status = this.selectedStatuses.join(',');
     Payload.projectList.publishDateRange = (this.publishStartDate.value && this.publishEndDate.value) ? `${this.publishStartDate.value.year}-${this.publishStartDate.value.month}-${this.publishStartDate.value.day} , ${this.publishEndDate.value.year}-${this.publishEndDate.value.month}-${this.publishEndDate.value.day}` : '';
     Payload.projectList.SubmissionDueDateRange = (this.submissionStartDate.value && this.submissionEndDate.value) ? `${this.submissionStartDate.value.year}-${this.submissionStartDate.value.month}-${this.submissionStartDate.value.day} , ${this.submissionEndDate.value.year}-${this.submissionEndDate.value.month}-${this.submissionEndDate.value.day}` : '';
     Payload.projectList.valueRange = this.minValue + '-' + this.maxValue;
